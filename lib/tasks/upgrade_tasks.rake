79b7475b0d116973b32680fa423977da57a12966
Ben Crouse <bcrouse@weblinc.com>
2016-01-27 14:17:06 -0500

    Start the report

--- lib/tasks/upgrade_tasks.rake
+++ lib/tasks/upgrade_tasks.rake
@@ -8,55 +8,5 @@ namespace :weblinc do
       Rake::Task['db:mongoid:create_indexes'].invoke
       Rake::Task['weblinc:search_index:all'].invoke
     end
-
-    desc 'Calculate a report card to gauge upgrade difficulty'
-    task :report_card do
-      from = ENV['FROM_VERSION']
-      to = ENV['TO_VERSION']
-
-      puts "This rake tasks requires ENV['FROM_VERSION'] to be defined." if from.blank?
-      puts "This rake tasks requires ENV['TO_VERSION'] to be defined." if to.blank?
-
-      if from.blank? || to.blank?
-        puts 'For example:'
-        puts "  rake weblinc:upgrade:report_card FROM_VERSION=0.12.6 TO_VERSION=2.0.3"
-        exit
-      end
-
-      require 'rails/generators'
-      require 'generators/weblinc/diff/diff_generator'
-
-      generator = Weblinc::Generators::DiffGenerator.new([from, to])
-      diff = Weblinc::Upgrade::Diff.new(from, to)
-      report_card = Weblinc::Upgrade::ReportCard.new(diff)
-
-      puts 'Diff Statistics'
-      puts '---------------'
-      generator.print_status('---', 'removed', 'red', diff.removed_files)
-      generator.print_status('+++', 'added', 'green', diff.added_files)
-      generator.print_status('>>>', 'overridden', 'yellow', diff.overridden_files)
-      generator.print_status('>>>', 'decorated', 'yellow', diff.decorated_files)
-
-      puts
-      puts 'Report Card'
-      puts '-----------'
-      report_card.results.each do |category, grade|
-        color = if grade.in?(%w(A B))
-                  :green
-                elsif grade == 'F'
-                  :red
-                else
-                  :yellow
-                end
-
-        generator.say_status grade, category, color
-      end
-      puts
-    end
-
-    desc 'Read the release notes for the current version'
-    task :release_notes do
-      # Task goes here
-    end
   end
 end
--- lib/weblinc/upgrade/cli.rb
+++ lib/weblinc/upgrade/cli.rb
@@ -7,10 +7,7 @@ module Weblinc
       option :full, type: :boolean, aliases: :f
       option :context, type: :numeric, aliases: :c
       def diff(to)
-        from_path = Bundler.load.specs.find { |s| s.name == 'weblinc' }.full_gem_path
-        to_path = "#{Gem.dir}/gems/weblinc-#{to}"
-        validate_installed_gems!(to_path)
-
+        to_path = find_to_path!(to)
         diff = Diff.new(from_path, to_path, context: options[:context])
 
         if options[:removed]
@@ -26,20 +23,58 @@ module Weblinc
 
       desc 'report TO_VERSION', 'Print a report on upgrading to TO_VERSION'
       def report(to)
-        puts "report #{to}"
+        to_path = find_to_path!(to)
+        diff = Diff.new(from_path, to_path, context: options[:context])
+        report_card = Weblinc::Upgrade::ReportCard.new(diff)
+
+        puts 'Diff Statistics'
+        puts '---------------'
+        say_status('>>>', "#{pluralize(diff.all.length, 'file')} changed in weblinc", :yellow)
+        say_status('---', "#{pluralize(diff.removed.length, 'file')} removed from weblinc", :red)
+        say_status('+++', "#{pluralize(diff.added.length, 'file')} added to weblinc", :green)
+        say_status('>>>', "#{pluralize(diff.overridden.length, 'overridden file')} in this app changed", :yellow)
+        say_status('>>>', "#{pluralize(diff.decorated.length, 'decorated file')} in this app changed", :yellow)
+
+        #puts
+        #puts 'Report Card'
+        #puts '-----------'
+        #report_card.results.each do |category, grade|
+          #color = if grade.in?(%w(A B))
+                    #:green
+                  #elsif grade == 'F'
+                    #:red
+                  #else
+                    #:yellow
+                  #end
+
+          #generator.say_status grade, category, color
+        #end
+        #puts
       end
 
       private
 
-      def validate_installed_gems!(path)
-        if !File.directory?(path)
+      def from_path
+        Bundler.load.specs.find { |s| s.name == 'weblinc' }.full_gem_path
+      end
+
+      def find_to_path!(arg)
+        result = "#{Gem.dir}/gems/weblinc-#{arg}"
+
+        if !File.directory?(result)
           raise <<-eos.strip_heredoc
 
-            Couldn't find the desired TO_VERSION in installed gems!
-            Looked in #{path}
-            Try `gem install weblinc -v TO_VERSION`.
+            Couldn't find the desired v#{arg} in installed gems!
+            Looked in #{result}
+            Try `gem install weblinc -v #{arg}`.
           eos
         end
+
+        result
+      end
+
+      def pluralize(*args)
+        ActionController::Base.helpers.pluralize(*args)
       end
     end
   end
